7d6c0e58456e0d7a3181ef7f6c762ea8da13b3f9
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2021-03-16 21:55:27 -0300

    Comiendo frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom';
+
+import Balance from './components/Balance';
+import Login from './components/Login';
+import NewOperation from './components/NewOperation';
+import ListAllOperations from './components/ListAllOperations';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route
+					exact path='/'
+					component={Balance}
+				/>
+
+				<Route
+					exact path = '/login'
+					component = {Login}
+				/>
+
+				<Route
+					exact path = '/new'
+					component = {NewOperation}
+				/>
+
+				<Route
+					exact path = '/all'
+					component = {ListAllOperations}
+				/>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/Balance.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Balance = () => {
+    return (
+        <h2>Balance</h2>
+    );
+}
+ 
+export default Balance;
